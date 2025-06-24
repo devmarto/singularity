d336daa37b0abd12cc5541ee7e09582bb0d2061b
devmarto <dev.marto87@gmail.com>
2025-06-24 15:40:39 -0300

    Create simple image link form component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,17 @@
 
 import Navigation from "./components/Navigation/Navigation"
 import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 function App() {
 
   return (
     <>
       <Navigation />
-      <Rank />
+      <div className="flex flex-col justify-center items-center h-[80vh]">
+        <Rank />
+        <ImageLinkForm />
+      </div>
     </>
   )
 }
