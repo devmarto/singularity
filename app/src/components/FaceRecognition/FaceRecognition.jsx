50dfbcd1f77671dacaf8c9606514100cfed630ee
devmarto <dev.marto87@gmail.com>
2025-06-27 16:38:00 -0300

    Create Face Detection Box

--- app/src/components/FaceRecognition/FaceRecognition.jsx
+++ app/src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,7 +1,25 @@
-const FaceRecognition = ({ image }) => {
+const FaceRecognition = ({ image, boxes }) => {
+  const colors = ['border-indigo-500', 'border-red-500', 'border-green-500', 'border-yellow-500', 'border-purple-500'];
+
   return (
-    <div className="max-w-2xl w-full h-auto pb-6">
-      <img className="rounded-lg" src={image} alt="" />
+    <div className="max-w-2xl w-full h-auto pb-6 relative">
+      <img id="imageUpload" className="rounded-lg w-full" src={image} alt="" />
+      {boxes && boxes.length > 0 && boxes.map((box) => (
+        <div 
+          key={box.id}
+          className={`absolute border-4 ${colors[box.id % colors.length]} pointer-events-none`}
+          style={{
+            left: `${box.left}px`,
+            top: `${box.top}px`,
+            width: `${box.width}px`,
+            height: `${box.height}px`
+          }}
+        >
+          <span className={`absolute -top-6 left-0 ${colors[box.id % colors.length].replace('border', 'bg')} text-white px-2 py-1 text-xs rounded`}>
+            Face {box.id + 1}
+          </span>
+        </div>
+      ))}
     </div>
   )
 }
