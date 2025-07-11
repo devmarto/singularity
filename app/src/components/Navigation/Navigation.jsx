c0a5488f78edb572616022afb1f6c63e0c052193
devmarto <dev.marto87@gmail.com>
2025-07-11 20:06:09 -0300

    Add error toast and fix login

--- app/src/components/Navigation/Navigation.jsx
+++ app/src/components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router";
 const Navigation = () => {
   return (
     <nav className="container flex justify-between items-center py-4">
-      <NavLink to="/">
+      <NavLink to="/home">
         <Logo align={'h-24'} />
       </NavLink>
       <div>
